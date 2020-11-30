d2c260158b27b3d49cf5bcc596d170626d965802
Cédric Delpoux <cedric.delpoux@gmail.com>
2020-11-30 13:45:01 +0000

    fix transformStravaActivity

--- src/utils/node/transform-strava.js
+++ src/utils/node/transform-strava.js
@@ -1,7 +1,7 @@
 const mapboxPolyline = require("@mapbox/polyline")
 
 exports.transformStravaActivity = (activity) => {
-  if (activity.map) {
+  if (activity.map && activity.map.summary_polyline) {
     activity.map.geoJSON = mapboxPolyline.toGeoJSON(
       activity.map.summary_polyline
     )
