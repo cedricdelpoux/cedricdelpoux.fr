08fe71215aec18b1bdad3129448c62c4d11a88aa
Cédric Delpoux <cedric.delpoux@gmail.com>
2021-01-02 08:01:46 +0100

    add border-radius to iframes

--- src/components/html.js
+++ src/components/html.js
@@ -89,6 +89,7 @@ export const Html = ({html, css, fluid, ...props}) => {
         },
         "& > p > iframe": {
           maxWidth: "100%",
+          borderRadius: 2,
         },
         "&& > * + *": {
           mt: 2,
