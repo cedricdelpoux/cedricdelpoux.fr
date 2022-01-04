f567dd67662e126463069514be55d0f49c411c66
Cédric Delpoux <cedric.delpoux@gmail.com>
2022-01-04 09:36:20 +0100

    feat: make Grid works with MDX images

--- src/components/grid.js
+++ src/components/grid.js
@@ -10,6 +10,9 @@ export const Grid = ({children, css, ...props}) => {
         gridTemplateColumns: "repeat(auto-fill, minmax(300px,1fr))",
         columnGap: 2,
         rowGap: 2,
+        "& > *": {
+          width: "100%",
+        },
         ...css,
       }}
       {...props}
