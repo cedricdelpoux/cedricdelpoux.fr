6c5ae3c21d4946417c310f73e1c50ecb1207873a
Cédric Delpoux <cedric.delpoux@gmail.com>
2022-04-09 05:37:37 +0200

    feat: add Italy flag

--- src/components/flag.js
+++ src/components/flag.js
@@ -1,3 +1,4 @@
+import React from "react"
 import FlagAT from "wobblecode-flat-flags/images/png-47/AT.png"
 import FlagBA from "wobblecode-flat-flags/images/png-47/BA.png"
 import FlagES from "wobblecode-flat-flags/images/png-47/ES.png"
@@ -7,11 +8,11 @@ import FlagHR from "wobblecode-flat-flags/images/png-47/HR.png"
 import FlagHU from "wobblecode-flat-flags/images/png-47/HU.png"
 import FlagID from "wobblecode-flat-flags/images/png-47/ID.png"
 import FlagIS from "wobblecode-flat-flags/images/png-47/IS.png"
+import FlagIT from "wobblecode-flat-flags/images/png-47/IT.png"
 import FlagJO from "wobblecode-flat-flags/images/png-47/JO.png"
 import FlagNL from "wobblecode-flat-flags/images/png-47/NL.png"
 import FlagNZ from "wobblecode-flat-flags/images/png-47/NZ.png"
 import FlagPE from "wobblecode-flat-flags/images/png-47/PE.png"
-import React from "react"
 
 import {View} from "./view"
 
@@ -25,6 +26,7 @@ const flags = {
   hungary: FlagHU,
   iceland: FlagIS,
   indonesia: FlagID,
+  italy: FlagIT,
   jordan: FlagJO,
   netherlands: FlagNL,
   peru: FlagPE,
