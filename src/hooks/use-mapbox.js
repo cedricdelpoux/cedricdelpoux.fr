7e953efc30519c73de85bf771bc752d65117ff5f
Cédric Delpoux <cedric.delpoux@gmail.com>
2022-04-20 18:42:58 +0200

    feat: add style param to  useMapbox

--- src/hooks/use-mapbox.js
+++ src/hooks/use-mapbox.js
@@ -1,16 +1,17 @@
+import {useSwitchTheme} from "@css-system/gatsby-plugin-css-system"
 import {ThemeContext} from "css-system"
 import {useContext} from "react"
-import {useSwitchTheme} from "@css-system/gatsby-plugin-css-system"
 
 const TOKEN = process.env.GATSBY_MAPBOX_TOKEN
 const THEME_LIGHT = "ckhj9ux1x2w7x19odqmtxccea"
 const THEME_DARK = "ckhjauub03fyd19otcjzj5qnw"
 
-export const useMapbox = (polyline) => {
+export const useMapbox = (polyline, style) => {
   const theme = useContext(ThemeContext)
   const [themeKey] = useSwitchTheme()
   const pathColor = theme.colors.secondary.substr(1)
   const polylineEncoded = encodeURIComponent(polyline)
-  const style = `xuopled/${themeKey === "light" ? THEME_LIGHT : THEME_DARK}`
-  return `https://api.mapbox.com/styles/v1/${style}/static/path+${pathColor}(${polylineEncoded})/auto/500x300@2x?access_token=${TOKEN}&logo=false&attribution=false`
+  const themeStyle =
+    style || `xuopled/${themeKey === "light" ? THEME_LIGHT : THEME_DARK}`
+  return `https://api.mapbox.com/styles/v1/${themeStyle}/static/path+${pathColor}(${polylineEncoded})/auto/500x300@2x?access_token=${TOKEN}&logo=false&attribution=false`
 }
