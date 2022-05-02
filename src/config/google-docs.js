1033cee52744c7e0bee01d3c56a0dac6e6752e14
Cédric Delpoux <cedric.delpoux@gmail.com>
2022-05-02 09:50:20 +0200

    chore: update gatsby-source-google-docs config

--- src/config/google-docs.js
+++ src/config/google-docs.js
@@ -2,6 +2,10 @@ exports.googleDocsOptions = {
   debug: process.env.DEBUG,
   folder: process.env.GOOGLE_DOCS_FOLDER,
   skipImages: process.env.MINIMAL ? true : false,
+  imagesOptions: {
+    width: 680,
+    height: 400,
+  },
   createPages: true,
   pageContext: ["locale", "category", "country", "region"],
 }
